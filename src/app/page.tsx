180aded393224aadd353957ea65e46ddce616705
Chee Diep <c.diep@hotmail.com>
2025-02-08 15:54:58 +0000

    Implement mobile card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,46 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start px-4 py-12">
+      <Card className="relative">
+        <div className="rounded-full border overflow-hidden h-[53px] w-[53px] absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <picture>
+            <source
+              srcSet="/images/space-meow-sm.webp"
+              media="(max-width: 768px)"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <img
+              src="/images/space-meow.webp"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </picture>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <CardContent className="text-center relative pb-8 pt-10">
+          <CardTitle>
+            <h1>Your next delivery for Dorian and Ocie</h1>
+          </CardTitle>
+          <p className="text-sm mb-4">
+            Hey Kayleigh! In two days' time, we'll be charging you for your next
+            order for Dorian and Ocie's fresh food.
+          </p>
+          <strong className="text-sm">Total price: £134.00</strong>
+          <div className="mt-6 w-full flex gap-4 justify-center">
+            <Button size="sm" asChild>
+              <Link href="#">See details</Link>
+            </Button>
+            <Button size="sm" variant="outline">
+              <Link href="#">Edit delivery</Link>
+            </Button>
+          </div>
+          <div className="w-fit text-sm uppercase font-bold border border-[#E7AADA] bg-[#FFB8EF] text-[#773E75] px-3 py-1 rotate-[-4.75deg] absolute -bottom-4 left-1/2 -translate-x-1/2">
+            Free gift
+          </div>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
