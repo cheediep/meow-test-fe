4590bcca2539a175d34e9bbd164e50cb1482a601
Chee Diep <9450433+cheediep@users.noreply.github.com>
2025-02-09 09:28:52 +0000

    Feature: Next delivery component (#1)
    * Add next files to gitignore
    
    * Implement mobile card
    
    * Implement card

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "rounded-lg border bg-card text-card-foreground",
+        className
+      )}
+      {...props}
+    />
+  );
+});
+Card.displayName = "Card";
+
+const CardTitle = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("font-bold text-primary", className)}
+    {...props}
+  />
+));
+CardTitle.displayName = "CardTitle";
+
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("px-4 py-6", className)} {...props} />
+));
+CardContent.displayName = "CardContent";
+
+export { Card, CardContent, CardTitle };
